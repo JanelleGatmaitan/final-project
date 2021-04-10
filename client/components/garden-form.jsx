8110d6f2a91057244a1754dc45dde0650a8b7579
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-04-09 17:56:19 -0700

    changing the form should update state to keep track of the user's input #3

--- client/components/garden-form.jsx
+++ client/components/garden-form.jsx
@@ -3,13 +3,24 @@ import React from 'react';
 export default class GardenForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = [];
+    this.state = {
+      soil: ' ',
+      sun: ' ',
+      size: ' ',
+      notes: ' '
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    console.log(event.target);
+    this.setState({
+      soil: event.target.value,
+      sun: event.target.value,
+      size: event.target.value,
+      notes: event.target.value
+    });
+    console.log('this.state', this.state);
   }
 
   handleSubmit(event) {
@@ -23,13 +34,13 @@ export default class GardenForm extends React.Component {
           <h4 className="garden-form-title">Create New Garden</h4>
           <form className="garden-form">
             <label htmlFor="soil">Soil Quality:</label>
-            <input type="text" id="soil" name="soil" /><br />
+            <input type="text" id="soil" name="soil" onChange={this.handleChange} value={this.state.soil} /><br />
             <label htmlFor="sun">Sun Exposure:</label>
-            <input type="text" id="sun" name="sun" /><br />
+            <input type="text" id="sun" name="sun" onChange={this.handleChange} value={this.state.sun}/><br />
             <label htmlFor="size">Size:</label>
-            <input type="text" id="size" name="size" /><br />
+            <input type="text" id="size" name="size" onChange={this.handleChange} value={this.state.size}/><br />
             <label htmlFor="notes">Notes</label><br />
-            <input type="text" id="notes" name="notes" /><br />
+            <input type="text" id="notes" name="notes" onChange={this.handleChange} value={this.state.notes} /><br />
             <button className="form-btn" onClick={this.handleSubmit}>Save</button>
          </form>
         </div>
