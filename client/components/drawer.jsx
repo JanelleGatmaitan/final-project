35d1f0063dfd9ce4afe8b5915eec37da9c6d4e78
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-04-14 16:57:18 -0700

    initialize state with {menuOpen: false} #8

--- client/components/drawer.jsx
+++ client/components/drawer.jsx
@@ -3,18 +3,34 @@ import React from 'react';
 export default class Drawer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      menuOpen: false
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.getMenuClass = this.getMenuClass.bind(this);
   }
 
   handleClick() {
     console.log('bars clicked');
+    this.setState({
+      menuOpen: true
+    });
+  }
+
+  getMenuClass() {
+    if (this.state.menuOpen) {
+      return 'shade';
+    }
+    return 'hidden';
   }
 
   render() {
     return (
       <div className="drawer">
         <i className="fas fa-bars drawer-icon" onClick={this.handleClick}></i>
+        <div className={this.getMenuClass()}>
+          <p>werwerrwer</p>
+        </div>
       </div>
     );
   }
