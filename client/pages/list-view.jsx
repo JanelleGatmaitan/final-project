13a396d7cc14dfe156c5200af87988eca6034ade
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-04-15 12:59:01 -0700

    make a function that returns a single plant in list view #8

--- client/pages/list-view.jsx
+++ client/pages/list-view.jsx
@@ -16,12 +16,39 @@ export default class ListView extends React.Component {
         this.setState({
           plantsInGarden: data
         });
-      });
+      })
+      .catch(err => console.error(err));
   }
 
   render() {
     return (
-      <p>adfasdfsdf</p>
+      <ul className="garden">
+        {
+          this.state.plantsInGarden.map(plant => (
+            <li key={plant.id}>
+              <SavedPlant plant={plant} />
+            </li>
+          ))
+        }
+      </ul>
     );
   }
+}
+
+function SavedPlant(props) {
+  return (
+    <div className="saved-plant">
+      <div className="column">
+        <img src="/images/tomatoes.jpg" className="list-img" alt="vegetable"></img>
+      </div>
+      <div className="text-column column">
+        <p className="list-text">Tomatoes</p>
+        <p className="list-text">Date</p>
+        <p className="list-text">Expected harvest</p>
+      </div>
+      <div className="column">
+        <i className="fas fa-times"></i>
+      </div>
+    </div>
+  );
 }
