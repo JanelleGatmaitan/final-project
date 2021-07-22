d1292396b16d699d3fea2448878500f2b3e5cf6d
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-07-22 15:36:20 -0700

    dynamically populate garden form w gardenInfo #38

--- client/pages/list-view.jsx
+++ client/pages/list-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GardenForm from '../components/garden-form';
-import DeleteConfirmation from '../components/delete-confirmation';
+import DeleteModal from '../components/delete-confirmation';
 import AppContext from '../lib/app-context';
 import getLocalStorage from '../lib/get-localStorage';
 
@@ -153,7 +153,12 @@ export default class ListView extends React.Component {
     }
     return (
       <>
-        <DeleteConfirmation className={this.getDeleteModalClass()} clickYes={this.handleRemove} clickNo={this.cancelRemoval} />
+      <GardenForm
+        positioning="relative"
+        title="My Garden"
+        placeHolders={this.state.gardenInfo}
+      />
+        {/* <DeleteModal className={this.getDeleteModalClass()} clickYes={this.handleRemove} clickNo={this.cancelRemoval} />
         <GardenForm position="garden-form-center" title="My Garden" onSave={this.handleSave}
         values={this.state.gardenInfo} handleChange={this.handleChange} />
       <div className="tasks">
@@ -177,30 +182,30 @@ export default class ListView extends React.Component {
               </li>
             ))
           }
-        </ul>
+        </ul> */}
       </>
     );
   }
 }
 
-function SavedPlant(props) {
-  const { name, dateAdded, expectedHarvestDate, plantId } = props.plant;
-  return (
-    <div className="saved-plant-data">
-      <div className="column">
-        <a href={`#plants?plantId=${plantId}`}>
-          <img src={`/images/${name.toLowerCase()}.jpg`} className="list-img" alt="vegetable"></img>
-        </a>
-      </div>
-      <div className="text-column">
-        <a className='detail-link' href={`#plants?plantId=${plantId}`}>
-          <p className="list-text">{name}</p>
-        </a>
-        <p className="list-text">{`Date added: ${dateAdded}`}</p>
-        <p className="list-text">{`Expected harvest: ${expectedHarvestDate}`}</p>
-      </div>
-        <i plantid={plantId} className="delete-list fas fa-times"></i>
-    </div>
-  );
-}
+// function SavedPlant(props) {
+//   const { name, dateAdded, expectedHarvestDate, plantId } = props.plant;
+//   return (
+//     <div className="saved-plant-data">
+//       <div className="column">
+//         <a href={`#plants?plantId=${plantId}`}>
+//           <img src={`/images/${name.toLowerCase()}.jpg`} className="list-img" alt="vegetable"></img>
+//         </a>
+//       </div>
+//       <div className="text-column">
+//         <a className='detail-link' href={`#plants?plantId=${plantId}`}>
+//           <p className="list-text">{name}</p>
+//         </a>
+//         <p className="list-text">{`Date added: ${dateAdded}`}</p>
+//         <p className="list-text">{`Expected harvest: ${expectedHarvestDate}`}</p>
+//       </div>
+//         <i plantid={plantId} className="delete-list fas fa-times"></i>
+//     </div>
+//   );
+// }
 ListView.contextType = AppContext;
