561ebd6a6447f2f3f4dc4a884e53e5e29d2990c8
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-07-26 16:32:30 -0700

    title links to detail view #38

--- client/components/list-view-plant-card.jsx
+++ client/components/list-view-plant-card.jsx
@@ -5,7 +5,8 @@ import {
   CloseButton,
   Image,
   Text,
-  Box
+  Box,
+  Link
 } from '@chakra-ui/react';
 
 // function SavedPlant(props) {
@@ -29,7 +30,7 @@ import {
 //   );
 // }
 function SavedPlant(props) {
-  const { name, dateAdded } = props.plant;
+  const { plantId, name, dateAdded } = props.plant;
   return (
     <>
       <Box
@@ -56,12 +57,17 @@ function SavedPlant(props) {
           mx="20px"
           />
           <Box>
-            <Heading
-            fontSize="16px"
-            mb="10px"
+            <Link
+            href={`#plants?plantId=${plantId}`}
+            rel="detail view"
             >
-              {name}
-            </Heading>
+              <Heading
+                fontSize="16px"
+                mb="10px"
+              >
+                {name}
+              </Heading>
+            </Link>
             <Text>{`Date added: ${dateAdded}`}</Text>
           </Box>
         </HStack>
