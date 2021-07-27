7f0069f1977f60193e62d4debe8315cc97031aac
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-07-27 16:21:26 -0700

    align search suggestions with search bar #44

--- client/components/delete-confirmation.jsx
+++ client/components/delete-confirmation.jsx
@@ -3,53 +3,55 @@ import {
   VStack,
   HStack,
   Button,
-  Heading
+  Heading,
+  Box
 } from '@chakra-ui/react';
 
 function DeleteModal(props) {
   return (
-    <VStack
-      padding="50px"
-      position="fixed"
-      maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '40vw' }}
-      bgColor="gray"
-      top="45%"
-      left="50%"
-      transform="translate(-50%, -50%)"
-      display={props.hide}
-      rounded="6px"
-    >
-      <Heading
-      align="center"
-      fontSize="18px"
-      mb="25px"
+    <Box className="shade" display={props.hide}>
+      <VStack
+        padding="50px"
+        position="fixed"
+        maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '40vw' }}
+        bgColor="gray"
+        top="45%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+        rounded="6px"
       >
-        Are you sure you want to delete this vegetable?
-      </Heading>
-      <HStack
-        justifyContent="space-around"
-      >
-        <Button
-        padding="20px"
-        width="88px"
-        bgColor="lightGreen"
-        _hover={{ bgColor: 'darkGreen' }}
-        onClick={props.clickYes}
-        plantid={props.key}
+        <Heading
+          align="center"
+          fontSize="18px"
+          mb="25px"
         >
-          Yes
-        </Button>
-        <Button
-          padding="20px"
-          width="88px"
-          bgColor="lightRed"
-          _hover={{ bgColor: 'darkRed' }}
-          onClick={props.clickNo}
+          Are you sure you want to delete this vegetable?
+        </Heading>
+        <HStack
+          justifyContent="space-around"
         >
-          No
-        </Button>
-      </HStack>
-    </VStack>
+          <Button
+            padding="20px"
+            width="88px"
+            bgColor="lightGreen"
+            _hover={{ bgColor: 'darkGreen' }}
+            onClick={props.clickYes}
+            plantid={props.key}
+          >
+            Yes
+          </Button>
+          <Button
+            padding="20px"
+            width="88px"
+            bgColor="lightRed"
+            _hover={{ bgColor: 'darkRed' }}
+            onClick={props.clickNo}
+          >
+            No
+          </Button>
+        </HStack>
+      </VStack>
+    </Box>
   );
 }
 
--- client/components/garden-form.jsx
+++ client/components/garden-form.jsx
@@ -13,95 +13,94 @@ import {
 
 function GardenForm(props) {
   return (
-    <FormControl
-      padding={5}
-      display={props.hide}
-      position={props.positioning.position}
-      top={props.positioning.top}
-      left={props.positioning.left}
-      transform={props.positioning.transform}
-    >
-      <VStack
-        bgColor="gray"
-        mx="auto"
-        padding='4'
-        borderRadius='lg'
-        maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '40vw' }}
-        width='100%'>
-        <Heading my={3}>{props.title}</Heading>
-        <FormControl
-          id="soil"
-        >
-          <FormLabel>Soil</FormLabel>
-          <Select
-            placeholder={props.placeHolders.soil}
-            onChange={props.handleChange}
-            name="soil"
+      <FormControl
+        padding={5}
+        position={props.positioning.position}
+        top={props.positioning.top}
+        left={props.positioning.left}
+        transform={props.positioning.transform}
+      >
+        <VStack
+          bgColor="gray"
+          mx="auto"
+          padding='4'
+          borderRadius='lg'
+          maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '40vw' }}
+          width='100%'>
+          <Heading my={3}>{props.title}</Heading>
+          <FormControl
+            id="soil"
           >
-            <option>Loamy</option>
-            <option>Clay</option>
-            <option>Sandy</option>
-            <option>Silty</option>
-            <option>Chalky</option>
-            <option>Peaky</option>
-          </Select>
-        </FormControl>
-        <FormControl id="sun">
-          <FormLabel>Sun</FormLabel>
-          <Select
-            placeholder={props.placeHolders.sun}
-            onChange={props.handleChange}
-            name="sun"
-          >
-            <option>Full sun</option>
-            <option>Part sun</option>
-            <option>Part shade</option>
-            <option>Full shade</option>
-          </Select>
-        </FormControl>
-        <FormControl id="size">
-          <FormLabel>Garden bed size</FormLabel>
-          <Input
-            onChange={props.handleChange}
-            placeholder={props.placeHolders.size}
-            name="size"
-          />
-        </FormControl>
-        <FormControl id="notes">
-          <FormLabel>Notes</FormLabel>
-          <Textarea
-            resize="none"
-            placeholder={props.placeHolders.notes}
-            onChange={props.handleChange}
-            name="notes"
-          >
-          </Textarea>
-        </FormControl>
-        <HStack>
-          <Button
-            type="submit"
-            my="15px"
-            bgColor="green"
-            color="white"
-            onClick={props.onSave}
-          >
-            Save
-          </Button>
-          <Button
-            bgColor="darkRed"
-            color="white"
-            display={() => {
-              if (!props.cancel) {
-                return 'none';
-              }
-            }}
-            onClick={props.cancel}
-          >
-            Cancel
-          </Button>
-        </HStack>
-      </VStack>
-    </FormControl>
+            <FormLabel>Soil</FormLabel>
+            <Select
+              placeholder={props.placeHolders.soil}
+              onChange={props.handleChange}
+              name="soil"
+            >
+              <option>Loamy</option>
+              <option>Clay</option>
+              <option>Sandy</option>
+              <option>Silty</option>
+              <option>Chalky</option>
+              <option>Peaky</option>
+            </Select>
+          </FormControl>
+          <FormControl id="sun">
+            <FormLabel>Sun</FormLabel>
+            <Select
+              placeholder={props.placeHolders.sun}
+              onChange={props.handleChange}
+              name="sun"
+            >
+              <option>Full sun</option>
+              <option>Part sun</option>
+              <option>Part shade</option>
+              <option>Full shade</option>
+            </Select>
+          </FormControl>
+          <FormControl id="size">
+            <FormLabel>Garden bed size</FormLabel>
+            <Input
+              onChange={props.handleChange}
+              placeholder={props.placeHolders.size}
+              name="size"
+            />
+          </FormControl>
+          <FormControl id="notes">
+            <FormLabel>Notes</FormLabel>
+            <Textarea
+              resize="none"
+              placeholder={props.placeHolders.notes}
+              onChange={props.handleChange}
+              name="notes"
+            >
+            </Textarea>
+          </FormControl>
+          <HStack>
+            <Button
+              type="submit"
+              my="15px"
+              bgColor="green"
+              color="white"
+              onClick={props.onSave}
+            >
+              Save
+            </Button>
+            <Button
+              bgColor="darkRed"
+              color="white"
+              display={() => {
+                if (!props.cancel) {
+                  return 'none';
+                }
+              }}
+              onClick={props.cancel}
+            >
+              Cancel
+            </Button>
+          </HStack>
+        </VStack>
+      </FormControl>
   );
 }
 
