20b1fc32bbe149d3217a964b8669b74ecd881696
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-07-27 22:01:00 -0700

    auth form made with chakra components #46

--- client/components/auth-form.jsx
+++ client/components/auth-form.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import AppContext from '../lib/app-context';
+import {
+  FormControl,
+  FormLabel,
+  Button,
+  Input,
+  VStack
+} from '@chakra-ui/react';
 
 export default class AuthForm extends React.Component {
   constructor(props) {
@@ -44,38 +51,71 @@ export default class AuthForm extends React.Component {
       submitBtnText = 'Sign In';
     }
     return (
-      <div className="auth-container">
-        <form className="auth-form">
-          <label htmlFor="username" className="auth-form-label">
-            Username
-          </label>
-          <input
-            required
-            autoFocus
-            id="username"
-            type="text"
-            name="username"
-            onChange={this.handleChange}
-            className="auth-row auth-form-control" />
-          <label htmlFor="password" className="auth-form-label">
-            Password
-          </label>
-          <input
-            required
-            autoFocus
-            id="password"
-            type="password"
-            name="password"
-            onChange={this.handleChange}
-            className="auth-row auth-form-control" />
-          <div className="auth-btn-container">
-            <button type="submit" className="auth-btn" onClick={this.handleSubmit}>
-              {submitBtnText}
-          </button>
-          </div>
-      </form>
-      </div>
+      <VStack
+      maxW="60vw"
+      margin="auto"
+      >
+        <FormControl id="username" mb="25px">
+          <FormLabel>Username</FormLabel>
+          <Input
+          name="username"
+          isRequired={true}
+          type="text"
+          onChange={this.handleChange}
+          />
+        </FormControl>
+        <FormControl id="password">
+          <FormLabel>Password</FormLabel>
+          <Input
+          type="password"
+          name="password"
+          onChange={this.handleChange}
+          />
+        </FormControl>
+        <Button
+          type="submit"
+          my="15px"
+          bgColor="green"
+          color="white"
+          onClick={this.handleSubmit}
+        >
+          {submitBtnText}
+        </Button>
+      </VStack>
     );
+    // return (
+    //   <div className="auth-container">
+    //     <form className="auth-form">
+    //       <label htmlFor="username" className="auth-form-label">
+    //         Username
+    //       </label>
+    //       <input
+    //         required
+    //         autoFocus
+    //         id="username"
+    //         type="text"
+    //         name="username"
+    // onChange={this.handleChange}
+    //         className="auth-row auth-form-control" />
+    //       <label htmlFor="password" className="auth-form-label">
+    //         Password
+    //       </label>
+    //       <input
+    //         required
+    //         autoFocus
+    //         id="password"
+    //         type="password"
+    //         name="password"
+    //         onChange={this.handleChange}
+    //         className="auth-row auth-form-control" />
+    //       <div className="auth-btn-container">
+    //         <button type="submit" className="auth-btn" onClick={this.handleSubmit}>
+    //           {submitBtnText}
+    //       </button>
+    //       </div>
+    //   </form>
+    //   </div>
+    // );
   }
 }
 
